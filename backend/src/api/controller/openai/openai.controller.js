ee1c83f38dc9efa9e36a5e542848d9db9d58009d
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2023-12-27 10:49:50 -0300

    Construção do prompt + tratamento do retorno da api

--- backend/src/api/controller/openai/openai.controller.js
+++ backend/src/api/controller/openai/openai.controller.js
@@ -1,19 +1,46 @@
-import GenericController from '../generic.controller.js';
-import openaiService from '../../../service/openai/openai.service.js';
+import GenericController from "../generic.controller.js";
+import openaiService from "../../../service/openai/openai.service.js";
+import { template } from "./openai.template.js";
+import { getAttributeValue } from "../../../utils/utils.js";
 
 class OpenaiController extends GenericController {
     constructor() {
         super();
+        this._openaiService = openaiService;
+        this._template = template;
+    }
 
-        this._openaiService = openaiService
+    async buildPrompt(template, data) {
+        template["food"] = getAttributeValue(data, "food", "Almoço");
+        template["weight"] = getAttributeValue(data, "weight", 0);
+        template["height"] = getAttributeValue(data, "height", 0);
+        template["goal"] = getAttributeValue(data, "goal", "Manter peso");
+        template["goalKgs"] = getAttributeValue(data, "goalKgs", 0);
+        template["activityLevel"] = getAttributeValue(data, "activityLevel", "Moderado");
+        template["restriction"] = getAttributeValue(data, "restriction", "Sem restrição");
+        template["preference"] = getAttributeValue(data, "preference", "Sem preferência");
+
+        return template;
     }
 
     async runPrompt(req, res, next) {
         try {
-            const response = await this._openaiService.runPrompt(prompt);
-            this.sendSuccessResponse(res, 'Sucesso', {})
+            let jsonResponse = {};
+            const body = req.body || {};
+
+            const prompt = await this.buildPrompt(this._template, body);
+            const promptString = JSON.stringify(prompt);
+
+            const response = await this._openaiService.runPrompt(promptString);
+
+            if (response.choices[0].text) {
+                jsonResponse = JSON.parse(response.choices[0].text);
+                console.log(jsonResponse);
+            } else throw new Error("Unable to get a response");
+
+            this.sendSuccessResponse(res, "Sucesso", jsonResponse);
         } catch (error) {
-            this.sendErrorResponse(res, error, {})
+            this.sendErrorResponse(res, error, {});
         }
     }
 }
