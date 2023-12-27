d4e77917a21ed4f17de4d3ce7478d9a460087eb1
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2023-12-27 10:50:59 -0300

    Tratamento de erros na rota de criação de usuário

--- backend/src/api/controller/user/user.routes.js
+++ backend/src/api/controller/user/user.routes.js
@@ -3,6 +3,12 @@ import userController from "../user/user.controller.js";
 
 const router = Router({ mergeParams: true });
 
-router.post('/', (req, res, next) => userController.createUser(req, res, next));
+router.post('/', async (req, res, next) => {
+    try {
+        await userController.createUser(req, res, next)
+    } catch (error) {
+        userController.sendErrorResponse(res, { status: 500, message: 'Ocorreu um erro inesperado', error });
+    }
+});
 
 export default router;
