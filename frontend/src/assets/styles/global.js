5187f22c37d621dd85c4b679de53d78292c72271
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2024-01-10 21:22:18 -0300

    Ajuste no estilo global da aplicação

--- frontend/src/assets/styles/global.js
+++ frontend/src/assets/styles/global.js
@@ -5,8 +5,8 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Encode Sans Expanded', sans-serif;
+    font-family: 'Nunito Sans', sans-serif;
     color: #302C2B;
-    background-color: #F6F6F6;
+    background-color: #FFFFFF;
   }
 `;
