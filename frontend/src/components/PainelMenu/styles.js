f0d1b281234743fd71a771531cf0dd5c8dcae710
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2024-01-21 14:00:58 -0300

    Adaptação do painel principal do dahboard

---
+++ frontend/src/components/PainelMenu/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const BoxDashboard = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: transparent;
+    padding: 62px;
+`;
+
+export const CardContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 58px 0px;
+    background-color: transparent;
+`;
+
+export const BoxInfoMenu = styled.div`
+    width: 40%;
+    height: 100%;
+    border-left: 1px solid #d9d9d9;
+`;
+
+export const ListInfoMenu = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 42px;
+    background-color: transparent;
+`;
