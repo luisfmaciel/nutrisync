81be74da31747edb6ac2c252a11898dc63bfb2a9
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2024-01-21 14:02:27 -0300

    Ajuste no painel do dashboard

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,19 +1,28 @@
 import Sidebar from "../../components/Sidebar";
 import { itemsMenu } from "../../components/Sidebar/itemsMenu";
 import Topbar from "../../components/Topbar";
-import Painel from "../../components/Painel";
+import Painel from "../../components/PainelMenu";
 import { DashboardContainer, Wrapper } from "./styles";
+import SpinnerDefault from "../../components/SpinnerDefault";
+import { useState } from "react";
 
 const Dashboard = () => {
+    const [loading, setLoading] = useState(true);
+
+    setTimeout(() => {
+        setLoading(false);
+    }, 2000);
+
     return (
         <DashboardContainer>
+            <SpinnerDefault loading={loading} />
             <Sidebar itemsMenu={itemsMenu} />
             <Wrapper>
-                <Topbar />
+                <Topbar searchBar={true} />
                 <Painel />
             </Wrapper>
         </DashboardContainer>
-    )
-} 
+    );
+};
 
 export default Dashboard;
