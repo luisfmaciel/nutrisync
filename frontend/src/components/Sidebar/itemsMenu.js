2d34540cd6acd01eb54f352fcd01784732dd08d9
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2024-02-03 17:47:49 -0300

    Acrescentando o atriuto de active no item do menu

--- frontend/src/components/Sidebar/itemsMenu.js
+++ frontend/src/components/Sidebar/itemsMenu.js
@@ -2,21 +2,26 @@ export const itemsMenu = [
     {
         path: "/dashboard/meus-cardapios",
         name: "Meus cardápios",
+        active: false
     },
     {
         path: "/dashboard/novo-cardapio",
         name: "Novo cardápio",
+        active: false
     },
     {
         path: "/dashboard/evolucao",
         name: "Evolução",
+        active: false
     },
     {
         path: "/dashboard/calculadora-imc",
         name: "Calculadora IMC",
+        active: false
     },
     {
         path: "/dashboard/perfil",
         name: "Perfil",
+        active: false
     },
 ];
