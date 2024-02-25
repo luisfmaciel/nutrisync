0a718fa5b2a07a1d0f37133dae65194620928266
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2024-02-25 13:50:14 -0300

    Inclusão das rotas do cardápio

--- backend/src/api/controller/routes.controller.js
+++ backend/src/api/controller/routes.controller.js
@@ -1,9 +1,11 @@
 import loginRoutes from './login/login.routes.js';
 import userRoutes from './user/user.routes.js';
 import openaiRoutes from './openai/openai.routes.js';
+import menuRoutes from './menu/menu.routes.js';
 
 export {
     loginRoutes,
     userRoutes,
-    openaiRoutes
+    openaiRoutes,
+    menuRoutes
 };
--- backend/src/api/routes.js
+++ backend/src/api/routes.js
@@ -1,10 +1,11 @@
-import { loginRoutes, userRoutes, openaiRoutes } from './controller/routes.controller.js'
+import { loginRoutes, userRoutes, openaiRoutes, menuRoutes } from './controller/routes.controller.js'
 
 export default function (app) {
     
     app.use('/api/signin', loginRoutes);
     app.use('/api/register', userRoutes);
     app.use('/api/openai', openaiRoutes);
+    app.use('/api/menu', menuRoutes);
 
     app.use((err, _req, res, _next) => {
         if(err) res.status(500).json({});
