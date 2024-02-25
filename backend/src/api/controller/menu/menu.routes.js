a1d4e994884458c69adcc0f2fa13c0037283c9f2
Luis Felipe Maciel <luis.barbosa@al.infnet.edu.br>
2024-02-25 13:50:31 -0300

    inclusão das rotas do cardápio

---
+++ backend/src/api/controller/menu/menu.routes.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import menuController from "./menu.controller.js";
+
+const router = Router({ mergeParams: true });
+
+router.get('/', async (req, res) => {
+    try {
+        await menuController.createMenu(req, res)
+    } catch (error) {
+        menuController.sendErrorResponse(res, { status: 500, message: 'Ocorreu um erro inesperado', error });
+    }
+});
+
+router.post('/create', async (req, res) => {
+    try {
+        await menuController.createMenu(req, res)
+    } catch (error) {
+        menuController.sendErrorResponse(res, { status: 500, message: 'Ocorreu um erro inesperado', error });
+    }
+});
+
+router.put('/updateClassification', async (req, res) => {
+    try {
+        await menuController.updateClassificationMenu(req, res)
+    } catch (error) {
+        menuController.sendErrorResponse(res, { status: 500, message: 'Ocorreu um erro inesperado', error });
+    }
+});
+
+router.delete('/delete', async (req, res) => {
+    try {
+        await menuController.deleteMenu(req, res)
+    } catch (error) {
+        menuController.sendErrorResponse(res, { status: 500, message: 'Ocorreu um erro inesperado', error });
+    }
+});
+
+export default router;
